f6b812cd94fc764a8d1f90ec51bc7769d6de912c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-11-28 08:20:35 +0100

    New entry begins

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,12 @@ import path from "path";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Welcome");
