107b5cf58b6a083bc361b2ddcf99de1dc2ac186c
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-11-28 08:16:47 +0100

    New entry begins

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Welcome");
+});
+
 app.listen(8080, () => console.log(`Server is up running on port 8080`));
