af7da3c2ce62550d67c1a51da86c86c7e1ebcf70
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-11-28 08:23:06 +0100

    New entry begins

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,14 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
 app.get("/", (req, res) => {
     res.send("Welcome");
 });
