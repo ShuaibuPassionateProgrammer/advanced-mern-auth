52879fabcc227824e3ccbd7c4ed903a7d3a16383
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-12-04 11:41:30 +0100

    Setting up the user controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+import bcryptjs from "bcryptjs";
+import crypto from "crypto";
+
+const login = async (req, res) => {
+    // logic goes here
+};
+const logout = async (req, res) => {
+    // logic goes here
+};
+const signup = async (req, res) => {
+    // logic goes here
+};
+const verifyEmail = async (req, res) => {
+    // logic goes here
+};
+const forgotPassword = async (req, res) => {
+    // logic goes here
+};
+const resetPassword = async (req, res) => {
+    // logic goes here
+};
+const checkAuth = async (req, res) => {
+    // logic goes here
+};
+
+export {
+    login,
+    logout,
+    signup,
+    verifyEmail,
+    forgotPassword,
+    resetPassword,
+    checkAuth
+};
