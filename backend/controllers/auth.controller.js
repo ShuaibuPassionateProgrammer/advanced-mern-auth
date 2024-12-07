742f99d4af8b08935d78dc097a5a239281329c06
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-12-07 13:30:37 -0800

    User login functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,36 @@
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import { User } from "../models/user.model.js";
 
 const login = async (req, res) => {
-    // logic goes here
+	const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+		if (!isPasswordValid) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+
+		generateTokenAndSetCookie(res, user._id);
+
+		user.lastLogin = new Date();
+		await user.save();
+
+		res.status(200).json({
+			success: true,
+			message: "Logged in successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		console.log("Error in login ", error);
+		res.status(400).json({ success: false, message: error.message });
+	}
 };
 
 const logout = async (req, res) => {
@@ -25,7 +52,7 @@ export const signup = async (req, res) => {
 			return res.status(400).json({ success: false, message: "User already exists" });
 		}
 
-		const hashedPassword = await bcryptjs.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, 10);
 		const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
 
 		const user = new User({
