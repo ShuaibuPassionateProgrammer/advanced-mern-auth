e96fe9881bf8a9bb62da7e4e49efc441e8b5e77d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2024-12-10 20:42:33 -0800

    Uuser forgot password functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -112,7 +112,7 @@ const forgotPassword = async (req, res) => {
 
 		res.status(200).json({ success: true, message: "Password reset link sent to your email" });
 	} catch (error) {
-		console.log("Error in forgotPassword ", error);
+		console.log(`Error in forgotPassword ${error}`);
 		res.status(500).json({ success: false, message: error?.message });
 	}
 };
