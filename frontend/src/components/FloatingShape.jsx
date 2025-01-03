04a11f769e01e27453cf8f979a58c1eb4ffee6cb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-01-03 07:27:45 -0800

    Create FloatingShape component

--- frontend/src/components/FloatingShape.jsx
+++ frontend/src/components/FloatingShape.jsx
@@ -1,9 +1,22 @@
 import { motion } from "framer-motion";
 
-const FloatingShape = () => {
+const FloatingShape = ({ color, size, top, left, delay }) => {
     return (
-        <>
-            <h1>Floating Shape Component</h1>
-        </>
+        <motion.div
+            className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl`}
+			style={{ top, left }}
+            animate={{
+                x: ["0%", "100%", "0%"],
+                y: ["0%", "100%", "0%"],
+                rotate: [0, 360]
+            }}
+            transition={{
+                duration: 20,
+                ease: "linear",
+                repeat: Infinity,
+                delay
+            }}
+            aria-hidden="true">
+        </motion.div>
     );
 };
