fee3481e0e127f25d2a9f4160c4078cb57306a98
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-01-04 08:13:15 -0800

    Create LoadingSpinner component

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -2,7 +2,9 @@ import { motion } from "framer-motion";
 
 const LoadingSpinner = () => {
     return (
-        <motion.div></motion.div>
+        <div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
+            {/* Simple Loading Spinner */}
+        </div>
     );
 };
 
