a211708332fbd59bbc30a2acdcd23f98de61c8da
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-01-05 10:31:52 -0800

    Create PasswordStrengthMeter component

---
+++ frontend/src/components/PasswordStrengthMeter.jsx
@@ -0,0 +1,9 @@
+const PasswordStrengthMeter = () => {
+    return (
+        <div className="mt-2">
+            <h1>Password Strength Meter</h1>
+        </div>
+    );
+};
+
+export default PasswordStrengthMeter;
