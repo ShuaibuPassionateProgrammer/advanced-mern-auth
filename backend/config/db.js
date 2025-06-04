473f22840c8beab34a32ca1f3cae4a886c0397a9
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-04 15:59:37 +0100

    feat: implement database connection logic in connectDB function

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {}
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+    }
     catch (error) {
         console.error("Error in database connection: " + error.message);
         process.exit(1);
